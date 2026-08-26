3f36c59f1b52cea54b67d3af9a2d85b79cc96b72
bastos9601 <bastoshugo01@gmail.com>
2026-08-26 16:00:52 -0500

    Fix: Remover setAppCacheEnabled obsoleto de MainActivity

--- app/src/main/java/com/ayni/minedu/MainActivity.java
+++ app/src/main/java/com/ayni/minedu/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends Activity {
         
         // Habilitar caché
         webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
-        webSettings.setAppCacheEnabled(true);
+        // Nota: setAppCacheEnabled() fue removido en APIs modernas de Android
         
         // Configuración de zoom
         webSettings.setSupportZoom(true);
